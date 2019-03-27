fb4b4849b7f0177cb98d0a3debe456b49dbe80c6
Konstantin Kolodiy <bonewell@yandex.ru>
2019-03-27 22:56:27 +0300

    Add mongo driver

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
-#include <gsl/gsl>
+
 #include <boost/asio.hpp>
+#include <gsl/gsl>
+#include <mongocxx/instance.hpp>
+#include <mongocxx/client.hpp>
 
 using namespace std;
-
 
 void get(gsl::owner<int*> x) {
     *x = 4;
@@ -11,6 +13,9 @@ void get(gsl::owner<int*> x) {
 
 int main()
 {
+    mongocxx::instance instance{};
+    mongocxx::client client{mongocxx::uri{}};
+
     cout << "Hello World!" << endl;
     get(new int{});
 
